6b21481eb64283c21dfe2ecc50d7728c1446cd37
Guilleuz <801618@unizar.es>
2023-03-13 21:20:58 +0100

    Finished the fuse_annotations.py script.

--- remove-unused-videos.py
+++ remove-unused-videos.py
@@ -1,12 +1,15 @@
 # Guillermo Enguita Lahoz 801618
 # Script to delete all of the dataset videos that are not annotated
 # It takes as imput a video folder, and will remove any video that is not listed in the vid_list.csv
+# The file vid_list.csv must exist in the folder
+
 # Execute using: python remove-unused-videos.py <video folder>
 
 import os
 import sys
 
 
+# Removes any video in the video_folder path that is not listed in the vid_list.csv file
 def remove_unused_videos(video_folder):
     vid_list_file = open(video_folder + '/vid_list.csv')
     annotated_videos = vid_list_file.readlines()
@@ -15,10 +18,11 @@ def remove_unused_videos(video_folder):
     for i in range(0, len(annotated_videos)):
         annotated_videos[i] = annotated_videos[i].strip()
 
+    # Check all the videos in the folder, and delete them if necessary
     for video in os.listdir(video_folder):
         if video not in annotated_videos and video != 'vid_list.csv':
             print(video, "removed")
-            # os.remove(video_folder + '/' + video)
+            os.remove(video_folder + '/' + video)
 
 
 if __name__ == "__main__":
--- video_annotations/fuse_annotations.py
+++ video_annotations/fuse_annotations.py
@@ -7,11 +7,51 @@
 # python fuse_annotations.py <epic's noun annotations> <epic's features folder> <epic's verb annotations>
 #   <bsh's noun annotations> <bsh's verbs annotations> <bsh's feature folder>
 
-# Furthermore, the feature file names will be changed to distinguish them from one another, adding the prefix "EK_" to
-# Epic Kitchens ones and "BSH_" to our own, both in the files and in the annotations.
+# Furthermore, it offers a function to add a prefix to all files in a folder
+
+import json
+import os
+import sys
 
 
+# Combines two annotation files, one from Epic Kitchens and the other from our dataset
+def combine_annotations(bsh_file, epic_file, output_file):
+    # Load noun annotations
+    epic_file_f = open(epic_file)
+    epic_dataset = json.load(epic_file_f)
+
+    bsh_file_f = open(bsh_file)
+    bsh_dataset = json.load(bsh_file_f)
+
+    # Change every annotation in the Epic Kitchens dataset to the training subset
+    for entry in epic_dataset['database']:
+        epic_dataset['database'][entry]['subset'] = 'training'
+
+    # Change every annotation in our dataset to the validation subset, and add it to Epic's annotation list
+    for entry in bsh_dataset['database']:
+        bsh_dataset['database'][entry]['subset'] = 'validation'
+        epic_dataset['database']['BSH_' + entry] = bsh_dataset['database'][entry]
+
+    # Output the combined annotations
+    output = open(output_file, 'w+')
+    json.dump(epic_dataset, output)
+
+
+# Combines noun and verb annotations
 def fuse_annotations(epic_noun_file, epic_verb_file, bsh_noun_file, bsh_verb_file):
+    combine_annotations(bsh_noun_file, epic_noun_file, 'noun_annotations.json')
+    combine_annotations(bsh_verb_file, epic_verb_file, 'verb_annotations.json')
 
 
-def change_video_names():
+# Adds the prefix 'prefix' to all the files found in 'folder'
+def change_file_names(folder, prefix):
+    for file in os.listdir(folder):
+        os.rename(folder + '/' + file, folder + '/' + prefix + file)
+
+
+def main():
+    fuse_annotations(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+
+if __name__ == "__main__":
+    main()
